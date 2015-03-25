4680dcdf3eb688f30381eb9a834656b8fcd164c3
Oleksandr Tymoshenko <gonzo@bluezbox.com>
2015-03-25 15:05:09 -0700

    Add ti,hwmods hack to determine device id

---
+++ sys/arm/ti/ti_hwmods.c
@@ -0,0 +1,83 @@
+/*-
+ * Copyright (c) 2015 Oleksandr Tymoshenko <gonzo@freebsd.org>
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ * 1. Redistributions of source code must retain the above copyright
+ *    notice, this list of conditions and the following disclaimer.
+ * 2. Redistributions in binary form must reproduce the above copyright
+ *    notice, this list of conditions and the following disclaimer in the
+ *    documentation and/or other materials provided with the distribution.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE AUTHOR ``AS IS'' AND ANY EXPRESS OR
+ * IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+ * WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+ * DISCLAIMED.  IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR ANY DIRECT,
+ * INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+ * (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+ * SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)
+ * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT,
+ * STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN
+ * ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ * POSSIBILITY OF SUCH DAMAGE.
+ *
+ * $FreeBSD$
+ */
+
+#include <sys/cdefs.h>
+__FBSDID("$FreeBSD$");
+
+#include <sys/param.h>
+#include <sys/systm.h>
+#include <sys/bus.h>
+#include <sys/kernel.h>
+
+#include <dev/fdt/fdt_common.h>
+#include <dev/ofw/openfirm.h>
+#include <dev/ofw/ofw_bus.h>
+#include <dev/ofw/ofw_bus_subr.h>
+
+#include <machine/bus.h>
+#include <machine/fdt.h>
+
+#include <arm/ti/ti_hwmods.h>
+
+int
+ti_hwmods_get_unit(device_t dev, const char *hwname)
+{
+	phandle_t node;
+	int len, hwlen, l;
+	char *name;
+	int unit;
+
+	unit = -1;
+	hwlen = strlen(hwname);
+
+	if ((node = ofw_bus_get_node(dev)) == 0)
+		return (-1);
+
+	if ((len = OF_getproplen(node, "ti,hwmods")) <= 0)
+		return (-1);
+
+	name = malloc(len + 1, M_DEVBUF, M_NOWAIT);
+	if (OF_getprop(node, "ti,hwmods", (void*)name, len) <= 0) {
+		free(name, M_DEVBUF);
+		return (-1);
+	}
+
+	while (len > 0) {
+		if (strncmp(hwname, name, hwlen) == 0) {
+			unit = (int)strtol(name + hwlen, NULL, 10);
+			break;
+		}
+		/* Slide to the next sub-string. */
+		l = strlen(name) + 1;
+		name += l;
+		len -= l;
+	}
+	
+	free(name, M_DEVBUF);
+	return (unit);
+}
