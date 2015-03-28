4be29dfa423ea94faac7c420c73880b4192327b9
Oleksandr Tymoshenko <gonzo@bluezbox.com>
2015-03-27 18:26:28 -0700

    Use standard gic decode procedure

--- sys/arm/ti/ti_common.c
+++ sys/arm/ti/ti_common.c
@@ -51,23 +51,6 @@ struct fdt_fixup_entry fdt_fixup_table[] = {
 	{ NULL, NULL }
 };
 
-#ifdef SOC_OMAP4
-static int
-fdt_gic_decode_ic(phandle_t node, pcell_t *intr, int *interrupt, int *trig,
-    int *pol)
-{
-
-	if (!fdt_is_compatible(node, "arm,gic"))
-		return (ENXIO);
-
-	*interrupt = fdt32_to_cpu(intr[0]);
-	*trig = INTR_TRIGGER_CONFORM;
-	*pol = INTR_POLARITY_CONFORM;
-
-	return (0);
-}
-#endif
-
 #ifdef SOC_TI_AM335X
 static int
 fdt_aintc_decode_ic(phandle_t node, pcell_t *intr, int *interrupt, int *trig,
@@ -88,7 +71,7 @@ fdt_aintc_decode_ic(phandle_t node, pcell_t *intr, int *interrupt, int *trig,
 
 fdt_pic_decode_t fdt_pic_table[] = {
 #ifdef SOC_OMAP4
-	&fdt_gic_decode_ic,
+	&gic_decode_fdt,
 #endif
 #ifdef SOC_TI_AM335X
 	&fdt_aintc_decode_ic,
