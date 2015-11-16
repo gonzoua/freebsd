8f2b4cfe734e067b94f3efe1a7e3f8a40aba7a1f
Oleksandr Tymoshenko <gonzo@bluezbox.com>
2015-11-15 19:59:58 -0800

    Get rid of static sc

--- sys/arm/freescale/imx/imx6_hdmi.c
+++ sys/arm/freescale/imx/imx6_hdmi.c
@@ -79,8 +79,6 @@ static struct ofw_compat_data compat_data[] = {
 	{NULL,	            0}
 };
 
-static struct imx_hdmi_softc *imx_hdmi_sc;
-
 static inline uint8_t
 RD1(struct imx_hdmi_softc *sc, bus_size_t off)
 {
@@ -95,32 +93,18 @@ WR1(struct imx_hdmi_softc *sc, bus_size_t off, uint8_t val)
 	bus_write_1(sc->sc_mem_res, off, val);
 }
 
-static uint8_t
-imx_hdmi_read_1(bus_size_t off)
-{
-
-	return RD1(imx_hdmi_sc, off);
-}
-
-static void
-imx_hdmi_write_1(bus_size_t off, uint8_t val)
-{
-
-	WR1(imx_hdmi_sc, off, val);
-}
-
 static void
 imx_hdmi_phy_wait_i2c_done(struct imx_hdmi_softc *sc, int msec)
 {
 	unsigned char val = 0;
 
-	val = imx_hdmi_read_1(HDMI_IH_I2CMPHY_STAT0) &
+	val = RD1(sc, HDMI_IH_I2CMPHY_STAT0) &
 	    (HDMI_IH_I2CMPHY_STAT0_DONE | HDMI_IH_I2CMPHY_STAT0_ERROR);
 	while (val == 0) {
 		DELAY(1000);
 		if (msec-- == 0)
 			return;
-		val = imx_hdmi_read_1(HDMI_IH_I2CMPHY_STAT0) &
+		val = RD1(sc, HDMI_IH_I2CMPHY_STAT0) &
 		    (HDMI_IH_I2CMPHY_STAT0_DONE | HDMI_IH_I2CMPHY_STAT0_ERROR);
 	}
 }
@@ -131,20 +115,20 @@ imx_hdmi_phy_i2c_write(struct imx_hdmi_softc *sc, unsigned short data,
 {
 
 	/* clear DONE and ERROR flags */
-	imx_hdmi_write_1(HDMI_IH_I2CMPHY_STAT0,
+	WR1(sc, HDMI_IH_I2CMPHY_STAT0,
 	    HDMI_IH_I2CMPHY_STAT0_DONE | HDMI_IH_I2CMPHY_STAT0_ERROR);
-	imx_hdmi_write_1(HDMI_PHY_I2CM_ADDRESS_ADDR, addr);
-	imx_hdmi_write_1(HDMI_PHY_I2CM_DATAO_1_ADDR, (unsigned char)(data >> 8));
-	imx_hdmi_write_1(HDMI_PHY_I2CM_DATAO_0_ADDR, (unsigned char)(data >> 0));
-	imx_hdmi_write_1(HDMI_PHY_I2CM_OPERATION_ADDR, HDMI_PHY_I2CM_OPERATION_ADDR_WRITE);
+	WR1(sc, HDMI_PHY_I2CM_ADDRESS_ADDR, addr);
+	WR1(sc, HDMI_PHY_I2CM_DATAO_1_ADDR, (unsigned char)(data >> 8));
+	WR1(sc, HDMI_PHY_I2CM_DATAO_0_ADDR, (unsigned char)(data >> 0));
+	WR1(sc, HDMI_PHY_I2CM_OPERATION_ADDR, HDMI_PHY_I2CM_OPERATION_ADDR_WRITE);
 	imx_hdmi_phy_wait_i2c_done(sc, 1000);
 }
 
 static void
 imx_hdmi_disable_overflow_interrupts(struct imx_hdmi_softc *sc)
 {
-	imx_hdmi_write_1(HDMI_IH_MUTE_FC_STAT2, HDMI_IH_MUTE_FC_STAT2_OVERFLOW_MASK);
-	imx_hdmi_write_1(HDMI_FC_MASK2,
+	WR1(sc, HDMI_IH_MUTE_FC_STAT2, HDMI_IH_MUTE_FC_STAT2_OVERFLOW_MASK);
+	WR1(sc, HDMI_FC_MASK2,
 	    HDMI_FC_MASK2_LOW_PRI | HDMI_FC_MASK2_HIGH_PRI);
 }
 
@@ -177,41 +161,41 @@ imx_hdmi_av_composer(struct imx_hdmi_softc *sc)
 		HDMI_FC_INVIDCONF_DVI_MODEZ_DVI_MODE :
 		HDMI_FC_INVIDCONF_DVI_MODEZ_HDMI_MODE);
 
-	imx_hdmi_write_1(HDMI_FC_INVIDCONF, inv_val);
+	WR1(sc, HDMI_FC_INVIDCONF, inv_val);
 
 	/* Set up horizontal active pixel region width */
-	imx_hdmi_write_1(HDMI_FC_INHACTV1, sc->sc_mode.hdisplay >> 8);
-	imx_hdmi_write_1(HDMI_FC_INHACTV0, sc->sc_mode.hdisplay);
+	WR1(sc, HDMI_FC_INHACTV1, sc->sc_mode.hdisplay >> 8);
+	WR1(sc, HDMI_FC_INHACTV0, sc->sc_mode.hdisplay);
 
 	/* Set up vertical blanking pixel region width */
-	imx_hdmi_write_1(HDMI_FC_INVACTV1, sc->sc_mode.vdisplay >> 8);
-	imx_hdmi_write_1(HDMI_FC_INVACTV0, sc->sc_mode.vdisplay);
+	WR1(sc, HDMI_FC_INVACTV1, sc->sc_mode.vdisplay >> 8);
+	WR1(sc, HDMI_FC_INVACTV0, sc->sc_mode.vdisplay);
 
 	/* Set up horizontal blanking pixel region width */
 	hblank = sc->sc_mode.htotal - sc->sc_mode.hdisplay;
-	imx_hdmi_write_1(HDMI_FC_INHBLANK1, hblank >> 8);
-	imx_hdmi_write_1(HDMI_FC_INHBLANK0, hblank);
+	WR1(sc, HDMI_FC_INHBLANK1, hblank >> 8);
+	WR1(sc, HDMI_FC_INHBLANK0, hblank);
 
 	/* Set up vertical blanking pixel region width */
 	vblank = sc->sc_mode.vtotal - sc->sc_mode.vdisplay;
-	imx_hdmi_write_1(HDMI_FC_INVBLANK, vblank);
+	WR1(sc, HDMI_FC_INVBLANK, vblank);
 
 	/* Set up HSYNC active edge delay width (in pixel clks) */
 	hbp = sc->sc_mode.htotal - sc->sc_mode.hsync_end;
-	imx_hdmi_write_1(HDMI_FC_HSYNCINDELAY1, hbp >> 8);
-	imx_hdmi_write_1(HDMI_FC_HSYNCINDELAY0, hbp);
+	WR1(sc, HDMI_FC_HSYNCINDELAY1, hbp >> 8);
+	WR1(sc, HDMI_FC_HSYNCINDELAY0, hbp);
 
 	/* Set up VSYNC active edge delay (in pixel clks) */
 	vbp = sc->sc_mode.vtotal - sc->sc_mode.vsync_end;
-	imx_hdmi_write_1(HDMI_FC_VSYNCINDELAY, vbp);
+	WR1(sc, HDMI_FC_VSYNCINDELAY, vbp);
 
 	hsync_len = (sc->sc_mode.hsync_end - sc->sc_mode.hsync_start);
 	/* Set up HSYNC active pulse width (in pixel clks) */
-	imx_hdmi_write_1(HDMI_FC_HSYNCINWIDTH1, hsync_len >> 8);
-	imx_hdmi_write_1(HDMI_FC_HSYNCINWIDTH0, hsync_len);
+	WR1(sc, HDMI_FC_HSYNCINWIDTH1, hsync_len >> 8);
+	WR1(sc, HDMI_FC_HSYNCINWIDTH0, hsync_len);
 
 	/* Set up VSYNC active edge delay (in pixel clks) */
-	imx_hdmi_write_1(HDMI_FC_VSYNCINWIDTH, (sc->sc_mode.vsync_end - sc->sc_mode.vsync_start));
+	WR1(sc, HDMI_FC_VSYNCINWIDTH, (sc->sc_mode.vsync_end - sc->sc_mode.vsync_start));
 }
 
 static void
@@ -219,10 +203,10 @@ imx_hdmi_phy_enable_power(struct imx_hdmi_softc *sc, uint8_t enable)
 {
 	uint8_t reg;
 
-	reg = imx_hdmi_read_1(HDMI_PHY_CONF0);
+	reg = RD1(sc, HDMI_PHY_CONF0);
 	reg &= ~HDMI_PHY_CONF0_PDZ_MASK;
 	reg |= (enable << HDMI_PHY_CONF0_PDZ_OFFSET);
-	imx_hdmi_write_1(HDMI_PHY_CONF0, reg);
+	WR1(sc, HDMI_PHY_CONF0, reg);
 }
 
 static void
@@ -230,10 +214,10 @@ imx_hdmi_phy_enable_tmds(struct imx_hdmi_softc *sc, uint8_t enable)
 {
 	uint8_t reg;
 
-	reg = imx_hdmi_read_1(HDMI_PHY_CONF0);
+	reg = RD1(sc, HDMI_PHY_CONF0);
 	reg &= ~HDMI_PHY_CONF0_ENTMDS_MASK;
 	reg |= (enable << HDMI_PHY_CONF0_ENTMDS_OFFSET);
-	imx_hdmi_write_1(HDMI_PHY_CONF0, reg);
+	WR1(sc, HDMI_PHY_CONF0, reg);
 }
 
 static void
@@ -241,10 +225,10 @@ imx_hdmi_phy_gen2_pddq(struct imx_hdmi_softc *sc, uint8_t enable)
 {
 	uint8_t reg;
 
-	reg = imx_hdmi_read_1(HDMI_PHY_CONF0);
+	reg = RD1(sc, HDMI_PHY_CONF0);
 	reg &= ~HDMI_PHY_CONF0_GEN2_PDDQ_MASK;
 	reg |= (enable << HDMI_PHY_CONF0_GEN2_PDDQ_OFFSET);
-	imx_hdmi_write_1(HDMI_PHY_CONF0, reg);
+	WR1(sc, HDMI_PHY_CONF0, reg);
 }
 
 static void
@@ -252,10 +236,10 @@ imx_hdmi_phy_gen2_txpwron(struct imx_hdmi_softc *sc, uint8_t enable)
 {
 	uint8_t reg;
 
-	reg = imx_hdmi_read_1(HDMI_PHY_CONF0);
+	reg = RD1(sc, HDMI_PHY_CONF0);
 	reg &= ~HDMI_PHY_CONF0_GEN2_TXPWRON_MASK;
 	reg |= (enable << HDMI_PHY_CONF0_GEN2_TXPWRON_OFFSET);
-	imx_hdmi_write_1(HDMI_PHY_CONF0, reg);
+	WR1(sc, HDMI_PHY_CONF0, reg);
 }
 
 static void
@@ -263,10 +247,10 @@ imx_hdmi_phy_sel_data_en_pol(struct imx_hdmi_softc *sc, uint8_t enable)
 {
 	uint8_t reg;
 
-	reg = imx_hdmi_read_1(HDMI_PHY_CONF0);
+	reg = RD1(sc, HDMI_PHY_CONF0);
 	reg &= ~HDMI_PHY_CONF0_SELDATAENPOL_MASK;
 	reg |= (enable << HDMI_PHY_CONF0_SELDATAENPOL_OFFSET);
-	imx_hdmi_write_1(HDMI_PHY_CONF0, reg);
+	WR1(sc, HDMI_PHY_CONF0, reg);
 }
 
 static void
@@ -274,10 +258,10 @@ imx_hdmi_phy_sel_interface_control(struct imx_hdmi_softc *sc, uint8_t enable)
 {
 	uint8_t reg;
 
-	reg = imx_hdmi_read_1(HDMI_PHY_CONF0);
+	reg = RD1(sc, HDMI_PHY_CONF0);
 	reg &= ~HDMI_PHY_CONF0_SELDIPIF_MASK;
 	reg |= (enable << HDMI_PHY_CONF0_SELDIPIF_OFFSET);
-	imx_hdmi_write_1(HDMI_PHY_CONF0, reg);
+	WR1(sc, HDMI_PHY_CONF0, reg);
 }
 
 static inline void
@@ -285,11 +269,11 @@ imx_hdmi_phy_test_clear(struct imx_hdmi_softc *sc, unsigned char bit)
 {
 	uint8_t val;
 
-	val = imx_hdmi_read_1(HDMI_PHY_TST0);
+	val = RD1(sc, HDMI_PHY_TST0);
 	val &= ~HDMI_PHY_TST0_TSTCLR_MASK;
 	val |= (bit << HDMI_PHY_TST0_TSTCLR_OFFSET) &
 		HDMI_PHY_TST0_TSTCLR_MASK;
-	imx_hdmi_write_1(HDMI_PHY_TST0, val);
+	WR1(sc, HDMI_PHY_TST0, val);
 }
 
 static void
@@ -299,12 +283,12 @@ imx_hdmi_clear_overflow(struct imx_hdmi_softc *sc)
 	uint8_t val;
 
 	/* TMDS software reset */
-	imx_hdmi_write_1(HDMI_MC_SWRSTZ, (uint8_t)~HDMI_MC_SWRSTZ_TMDSSWRST_REQ);
+	WR1(sc, HDMI_MC_SWRSTZ, (uint8_t)~HDMI_MC_SWRSTZ_TMDSSWRST_REQ);
 
-	val = imx_hdmi_read_1(HDMI_FC_INVIDCONF);
+	val = RD1(sc, HDMI_FC_INVIDCONF);
 
 	for (count = 0 ; count < 5 ; count++)
-		imx_hdmi_write_1(HDMI_FC_INVIDCONF, val);
+		WR1(sc, HDMI_FC_INVIDCONF, val);
 }
 
 static int
@@ -313,7 +297,7 @@ imx_hdmi_phy_configure(struct imx_hdmi_softc *sc)
 	uint8_t val;
 	uint8_t msec;
 
-	imx_hdmi_write_1(HDMI_MC_FLOWCTRL, HDMI_MC_FLOWCTRL_FEED_THROUGH_OFF_CSC_BYPASS);
+	WR1(sc, HDMI_MC_FLOWCTRL, HDMI_MC_FLOWCTRL_FEED_THROUGH_OFF_CSC_BYPASS);
 
 	/* gen2 tx power off */
 	imx_hdmi_phy_gen2_txpwron(sc, 0);
@@ -322,13 +306,13 @@ imx_hdmi_phy_configure(struct imx_hdmi_softc *sc)
 	imx_hdmi_phy_gen2_pddq(sc, 1);
 
 	/* PHY reset */
-	imx_hdmi_write_1(HDMI_MC_PHYRSTZ, HDMI_MC_PHYRSTZ_DEASSERT);
-	imx_hdmi_write_1(HDMI_MC_PHYRSTZ, HDMI_MC_PHYRSTZ_ASSERT);
+	WR1(sc, HDMI_MC_PHYRSTZ, HDMI_MC_PHYRSTZ_DEASSERT);
+	WR1(sc, HDMI_MC_PHYRSTZ, HDMI_MC_PHYRSTZ_ASSERT);
 
-	imx_hdmi_write_1(HDMI_MC_HEACPHY_RST, HDMI_MC_HEACPHY_RST_ASSERT);
+	WR1(sc, HDMI_MC_HEACPHY_RST, HDMI_MC_HEACPHY_RST_ASSERT);
 
 	imx_hdmi_phy_test_clear(sc, 1);
-	imx_hdmi_write_1(HDMI_PHY_I2CM_SLAVE_ADDR, HDMI_PHY_I2CM_SLAVE_ADDR_PHY_GEN2);
+	WR1(sc, HDMI_PHY_I2CM_SLAVE_ADDR, HDMI_PHY_I2CM_SLAVE_ADDR_PHY_GEN2);
 	imx_hdmi_phy_test_clear(sc, 0);
 
 	/*
@@ -406,14 +390,14 @@ imx_hdmi_phy_configure(struct imx_hdmi_softc *sc)
 
 	/*Wait for PHY PLL lock */
 	msec = 4;
-	val = imx_hdmi_read_1(HDMI_PHY_STAT0) & HDMI_PHY_TX_PHY_LOCK;
+	val = RD1(sc, HDMI_PHY_STAT0) & HDMI_PHY_TX_PHY_LOCK;
 	while (val == 0) {
 		DELAY(1000);
 		if (msec-- == 0) {
 			device_printf(sc->sc_dev, "PHY PLL not locked\n");
 			return (-1);
 		}
-		val = imx_hdmi_read_1(HDMI_PHY_STAT0) & HDMI_PHY_TX_PHY_LOCK;
+		val = RD1(sc, HDMI_PHY_STAT0) & HDMI_PHY_TX_PHY_LOCK;
 	}
 
 	return true;
@@ -441,27 +425,31 @@ imx_hdmi_enable_video_path(struct imx_hdmi_softc *sc)
 {
 	uint8_t clkdis;
 
-	/* control period minimum duration */
-	imx_hdmi_write_1(HDMI_FC_CTRLDUR, 12);
-	imx_hdmi_write_1(HDMI_FC_EXCTRLDUR, 32);
-	imx_hdmi_write_1(HDMI_FC_EXCTRLSPAC, 1);
+	/*
+	 * Control period timing
+	 * Values are minimal according to HDMI spec 1.4a
+	 */
+	WR1(sc, HDMI_FC_CTRLDUR, 12);
+	WR1(sc, HDMI_FC_EXCTRLDUR, 32);
+	WR1(sc, HDMI_FC_EXCTRLSPAC, 1);
 
-	/* Set to fill TMDS data channels */
-	imx_hdmi_write_1(HDMI_FC_CH0PREAM, 0x0B);
-	imx_hdmi_write_1(HDMI_FC_CH1PREAM, 0x16);
-	imx_hdmi_write_1(HDMI_FC_CH2PREAM, 0x21);
+	/*
+	 * Set to fill TMDS data channels
+	 */
+	WR1(sc, HDMI_FC_CH0PREAM, 0x0B);
+	WR1(sc, HDMI_FC_CH1PREAM, 0x16);
+	WR1(sc, HDMI_FC_CH2PREAM, 0x21);
 
 	/* Save CEC clock */
-	clkdis = imx_hdmi_read_1(HDMI_MC_CLKDIS) & HDMI_MC_CLKDIS_CECCLK_DISABLE;
+	clkdis = RD1(sc, HDMI_MC_CLKDIS) & HDMI_MC_CLKDIS_CECCLK_DISABLE;
 	clkdis |= ~HDMI_MC_CLKDIS_CECCLK_DISABLE;
 
 	/* Enable pixel clock and tmds data path */
-	clkdis = 0x7F & clkdis;
 	clkdis &= ~HDMI_MC_CLKDIS_PIXELCLK_DISABLE;
-	imx_hdmi_write_1(HDMI_MC_CLKDIS, clkdis);
+	WR1(sc, HDMI_MC_CLKDIS, clkdis);
 
 	clkdis &= ~HDMI_MC_CLKDIS_TMDSCLK_DISABLE;
-	imx_hdmi_write_1(HDMI_MC_CLKDIS, clkdis);
+	WR1(sc, HDMI_MC_CLKDIS, clkdis);
 }
 
 static void
@@ -478,69 +466,69 @@ imx_hdmi_video_packetize(struct imx_hdmi_softc *sc)
 	/* set the packetizer registers */
 	val = ((color_depth << HDMI_VP_PR_CD_COLOR_DEPTH_OFFSET) &
 		HDMI_VP_PR_CD_COLOR_DEPTH_MASK);
-	imx_hdmi_write_1(HDMI_VP_PR_CD, val);
+	WR1(sc, HDMI_VP_PR_CD, val);
 
-	val = imx_hdmi_read_1(HDMI_VP_STUFF);
+	val = RD1(sc, HDMI_VP_STUFF);
 	val &= ~HDMI_VP_STUFF_PR_STUFFING_MASK;
 	val |= HDMI_VP_STUFF_PR_STUFFING_STUFFING_MODE;
-	imx_hdmi_write_1(HDMI_VP_STUFF, val);
+	WR1(sc, HDMI_VP_STUFF, val);
 
-	val = imx_hdmi_read_1(HDMI_VP_CONF);
+	val = RD1(sc, HDMI_VP_CONF);
 	val &= ~(HDMI_VP_CONF_PR_EN_MASK |
 		HDMI_VP_CONF_BYPASS_SELECT_MASK);
 	val |= HDMI_VP_CONF_PR_EN_DISABLE |
 		HDMI_VP_CONF_BYPASS_SELECT_VID_PACKETIZER;
-	imx_hdmi_write_1(HDMI_VP_CONF, val);
+	WR1(sc, HDMI_VP_CONF, val);
 
-	val = imx_hdmi_read_1(HDMI_VP_STUFF);
+	val = RD1(sc, HDMI_VP_STUFF);
 	val &= ~HDMI_VP_STUFF_IDEFAULT_PHASE_MASK;
 	val |= 1 << HDMI_VP_STUFF_IDEFAULT_PHASE_OFFSET;
-	imx_hdmi_write_1(HDMI_VP_STUFF, val);
+	WR1(sc, HDMI_VP_STUFF, val);
 
-	imx_hdmi_write_1(HDMI_VP_REMAP, remap_size);
+	WR1(sc, HDMI_VP_REMAP, remap_size);
 
 	if (output_select == HDMI_VP_CONF_OUTPUT_SELECTOR_PP) {
-		val = imx_hdmi_read_1(HDMI_VP_CONF);
+		val = RD1(sc, HDMI_VP_CONF);
 		val &= ~(HDMI_VP_CONF_BYPASS_EN_MASK |
 			HDMI_VP_CONF_PP_EN_ENMASK |
 			HDMI_VP_CONF_YCC422_EN_MASK);
 		val |= HDMI_VP_CONF_BYPASS_EN_DISABLE |
 			HDMI_VP_CONF_PP_EN_ENABLE |
 			HDMI_VP_CONF_YCC422_EN_DISABLE;
-		imx_hdmi_write_1(HDMI_VP_CONF, val);
+		WR1(sc, HDMI_VP_CONF, val);
 	} else if (output_select == HDMI_VP_CONF_OUTPUT_SELECTOR_YCC422) {
-		val = imx_hdmi_read_1(HDMI_VP_CONF);
+		val = RD1(sc, HDMI_VP_CONF);
 		val &= ~(HDMI_VP_CONF_BYPASS_EN_MASK |
 			HDMI_VP_CONF_PP_EN_ENMASK |
 			HDMI_VP_CONF_YCC422_EN_MASK);
 		val |= HDMI_VP_CONF_BYPASS_EN_DISABLE |
 			HDMI_VP_CONF_PP_EN_DISABLE |
 			HDMI_VP_CONF_YCC422_EN_ENABLE;
-		imx_hdmi_write_1(HDMI_VP_CONF, val);
+		WR1(sc, HDMI_VP_CONF, val);
 	} else if (output_select == HDMI_VP_CONF_OUTPUT_SELECTOR_BYPASS) {
-		val = imx_hdmi_read_1(HDMI_VP_CONF);
+		val = RD1(sc, HDMI_VP_CONF);
 		val &= ~(HDMI_VP_CONF_BYPASS_EN_MASK |
 			HDMI_VP_CONF_PP_EN_ENMASK |
 			HDMI_VP_CONF_YCC422_EN_MASK);
 		val |= HDMI_VP_CONF_BYPASS_EN_ENABLE |
 			HDMI_VP_CONF_PP_EN_DISABLE |
 			HDMI_VP_CONF_YCC422_EN_DISABLE;
-		imx_hdmi_write_1(HDMI_VP_CONF, val);
+		WR1(sc, HDMI_VP_CONF, val);
 	} else {
 		return;
 	}
 
-	val = imx_hdmi_read_1(HDMI_VP_STUFF);
+	val = RD1(sc, HDMI_VP_STUFF);
 	val &= ~(HDMI_VP_STUFF_PP_STUFFING_MASK |
 		HDMI_VP_STUFF_YCC422_STUFFING_MASK);
 	val |= HDMI_VP_STUFF_PP_STUFFING_STUFFING_MODE |
 		HDMI_VP_STUFF_YCC422_STUFFING_STUFFING_MODE;
-	imx_hdmi_write_1(HDMI_VP_STUFF, val);
+	WR1(sc, HDMI_VP_STUFF, val);
 
-	val = imx_hdmi_read_1(HDMI_VP_CONF);
+	val = RD1(sc, HDMI_VP_CONF);
 	val &= ~HDMI_VP_CONF_OUTPUT_SELECTOR_MASK;
 	val |= output_select;
-	imx_hdmi_write_1(HDMI_VP_CONF, val);
+	WR1(sc, HDMI_VP_CONF, val);
 }
 
 static void
@@ -553,19 +541,19 @@ imx_hdmi_video_sample(struct imx_hdmi_softc *sc)
 	val = HDMI_TX_INVID0_INTERNAL_DE_GENERATOR_DISABLE |
 		((color_format << HDMI_TX_INVID0_VIDEO_MAPPING_OFFSET) &
 		HDMI_TX_INVID0_VIDEO_MAPPING_MASK);
-	imx_hdmi_write_1(HDMI_TX_INVID0, val);
+	WR1(sc, HDMI_TX_INVID0, val);
 
 	/* Enable TX stuffing: When DE is inactive, fix the output data to 0 */
 	val = HDMI_TX_INSTUFFING_BDBDATA_STUFFING_ENABLE |
 		HDMI_TX_INSTUFFING_RCRDATA_STUFFING_ENABLE |
 		HDMI_TX_INSTUFFING_GYDATA_STUFFING_ENABLE;
-	imx_hdmi_write_1(HDMI_TX_INSTUFFING, val);
-	imx_hdmi_write_1(HDMI_TX_GYDATA0, 0x0);
-	imx_hdmi_write_1(HDMI_TX_GYDATA1, 0x0);
-	imx_hdmi_write_1(HDMI_TX_RCRDATA0, 0x0);
-	imx_hdmi_write_1(HDMI_TX_RCRDATA1, 0x0);
-	imx_hdmi_write_1(HDMI_TX_BCBDATA0, 0x0);
-	imx_hdmi_write_1(HDMI_TX_BCBDATA1, 0x0);
+	WR1(sc, HDMI_TX_INSTUFFING, val);
+	WR1(sc, HDMI_TX_GYDATA0, 0x0);
+	WR1(sc, HDMI_TX_GYDATA1, 0x0);
+	WR1(sc, HDMI_TX_RCRDATA0, 0x0);
+	WR1(sc, HDMI_TX_RCRDATA1, 0x0);
+	WR1(sc, HDMI_TX_BCBDATA0, 0x0);
+	WR1(sc, HDMI_TX_BCBDATA1, 0x0);
 }
 
 static int
@@ -738,7 +726,6 @@ imx_hdmi_attach(device_t dev)
 	WR1(sc, HDMI_PHY_POL0, HDMI_PHY_HPD);
 	WR1(sc, HDMI_IH_PHY_STAT0, HDMI_IH_PHY_STAT0_HPD);
 
-	imx_hdmi_sc = sc;
 out:
 
 	if (err != 0)
