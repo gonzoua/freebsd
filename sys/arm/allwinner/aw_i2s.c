315479b43bf97428b910376fb6f4b15f7e76fbd1
Oleksandr Tymoshenko <gonzo@bluezbox.com>
2020-02-03 19:23:37 -0800

    Port the rest of AW i2s driver

--- sys/arm/allwinner/aw_i2s.c
+++ sys/arm/allwinner/aw_i2s.c
@@ -61,6 +61,7 @@ __FBSDID("$FreeBSD$");
 #define		DA_CTL_SDO_EN	(1 << 8)
 #define		DA_CTL_MS	(1 << 5)	/* sun4i */
 #define		DA_CTL_PCM	(1 << 4)	/* sun4i */
+#define		DA_CTL_MODE_SEL_MASK	(3 << 4) /* sun8i */
 #define		DA_CTL_MODE_SEL_PCM	(0 << 4) /* sun8i */
 #define		DA_CTL_MODE_SEL_LJ	(1 << 4) /* sun8i */
 #define		DA_CTL_MODE_SEL_RJ	(2 << 4) /* sun8i */
@@ -68,16 +69,17 @@ __FBSDID("$FreeBSD$");
 #define		DA_CTL_RXEN	(1 << 1)
 #define		DA_CTL_GEN	(1 << 0)
 #define	DA_FAT0		0x04
-#define		DA_FAT0_LRCK_PERIOD __BITS(17,8) /* sun8i */
-#define		DA_FAT0_LRCP	(1 << 7)
-#define		 DA_LRCP_NORMAL	0
-#define		 DA_LRCP_INVERTED	1
-#define		DA_FAT0_BCP	(1 << 6)
-#define		 DA_BCP_NORMAL		0
-#define		 DA_BCP_INVERTED	1
+#define		DA_FAT0_LRCK_PERIOD_MASK	(0x3ff << 8) /* sun8i */
+#define		DA_FAT0_LRCK_PERIOD(n)		(((n) & 0x3fff) << 8) /* sun8i */
+#define		DA_FAT0_LRCP_MASK	(1 << 7)
+#define		DA_LRCP_NORMAL		(0 << 7)
+#define		DA_LRCP_INVERTED	(1 << 7)
+#define		DA_FAT0_BCP_MASK	(1 << 6)
+#define		DA_BCP_NORMAL		(0 << 6)
+#define		DA_BCP_INVERTED		(1 << 6)
 #define		DA_FAT0_SR	__BITS(5,4)
 #define		DA_FAT0_WSS	__BITS(3,2)
-#define		DA_FAT0_FMT	__BITS(1,0)
+#define		DA_FAT0_FMT_MASK	(3 << 0)
 #define		 DA_FMT_I2S	0
 #define		 DA_FMT_LJ	1
 #define		 DA_FMT_RJ	2
@@ -98,11 +100,14 @@ __FBSDID("$FreeBSD$");
 #define	DA_CLKD		0x24
 #define		DA_CLKD_MCLKO_EN_SUN8I (1 << 8)
 #define		DA_CLKD_MCLKO_EN_SUN4I (1 << 7)
-#define		DA_CLKD_BCLKDIV_SUN8I __BITS(7,4)
-#define		DA_CLKD_BCLKDIV_SUN4I __BITS(6,4)
+#define		DA_CLKD_BCLKDIV_SUN8I(n)	(((n) & 0xf) << 4)
+#define		DA_CLKD_BCLKDIV_SUN8I_MASK	(0xf << 4)
+#define		DA_CLKD_BCLKDIV_SUN4I(n)	(((n) & 7) << 4)
+#define		DA_CLKD_BCLKDIV_SUN4I_MASK	(7 << 4)
 #define		 DA_CLKD_BCLKDIV_8	3
 #define		 DA_CLKD_BCLKDIV_16	5
-#define		DA_CLKD_MCLKDIV __BITS(3,0)
+#define		DA_CLKD_MCLKDIV(n)	(((n) & 0xff) << 0)
+#define		DA_CLKD_MCLKDIV_MASK	(0xf << 0)
 #define		 DA_CLKD_MCLKDIV_1	0
 #define	DA_TXCNT	0x28
 #define	DA_RXCNT	0x2c
@@ -114,7 +119,8 @@ __FBSDID("$FreeBSD$");
 #define		DA_CHCFG_TX_SLOT_NUM_MASK	(7 << 0)
 #define		DA_CHCFG_TX_SLOT_NUM(n)		(((n) & 7) << 0)
 
-#define	DA_CHSEL_OFFSET	__BITS(13,12)	/* sun8i */
+#define	DA_CHSEL_OFFSET(n)	(((n) & 3) << 12)	/* sun8i */
+#define	DA_CHSEL_OFFSET_MASK	(3 << 12)	/* sun8i */
 #define	DA_CHSEL_EN(n)		(((n) & 0xff) << 4)
 #define	DA_CHSEL_EN_MASK	(0xff << 4)
 #define	DA_CHSEL_SEL(n)		(((n) & 7) << 0)
@@ -122,7 +128,8 @@ __FBSDID("$FreeBSD$");
 
 #define	AUDIO_BUFFER_SIZE	48000 * 4
 
-#define	AW_I2S_SAMPLING_RATE	48000
+#define	AW_I2S_SAMPLE_RATE	48000
+#define	AW_I2S_CLK_RATE		24576000
 
 enum sunxi_i2s_type {
 	SUNXI_I2S_SUN4I,
@@ -154,6 +161,44 @@ static const struct sunxi_i2s_config sun8i_h3_config = {
 	.txchmap = 0x44,
 	.rxchsel = 0x54,
 	.rxchmap = 0x58,
+};
+
+static const u_int sun4i_i2s_bclk_divmap[] = {
+	[0] = 2,
+	[1] = 4,
+	[2] = 6,
+	[3] = 8,
+	[4] = 12,
+	[5] = 16,
+};
+
+static const u_int sun4i_i2s_mclk_divmap[] = {
+	[0] = 1,
+	[1] = 2,
+	[2] = 4,
+	[3] = 6,
+	[4] = 8,
+	[5] = 12,
+	[6] = 16,
+	[7] = 24,
+};
+
+static const u_int sun8i_i2s_divmap[] = {
+	[1] = 1,
+	[2] = 2,
+	[3] = 4,
+	[4] = 6,
+	[5] = 8,
+	[6] = 12,
+	[7] = 16,
+	[8] = 24,
+	[9] = 32,
+	[10] = 48,
+	[11] = 64,
+	[12] = 96,
+	[13] = 128,
+	[14] = 176,
+	[15] = 192,
 };
 
 
@@ -191,11 +236,23 @@ static int aw_i2s_probe(device_t dev);
 static int aw_i2s_attach(device_t dev);
 static int aw_i2s_detach(device_t dev);
 
+static u_int
+sunxi_i2s_div_to_regval(const u_int *divmap, u_int divmaplen, u_int div)
+{
+	u_int n;
+
+	for (n = 0; n < divmaplen; n++)
+		if (divmap[n] == div)
+			return n;
+
+	return -1;
+}
+
 static uint32_t sc_fmt[] = {
 	SND_FORMAT(AFMT_S16_LE, 2, 0),
 	0
 };
-static struct pcmchan_caps aw_i2s_caps = {AW_I2S_SAMPLING_RATE, AW_I2S_SAMPLING_RATE, sc_fmt, 0};
+static struct pcmchan_caps aw_i2s_caps = {AW_I2S_SAMPLE_RATE, AW_I2S_SAMPLE_RATE, sc_fmt, 0};
 
 
 static int
@@ -354,6 +411,8 @@ aw_i2s_dai_init(device_t dev, uint32_t format)
 {
 	struct aw_i2s_softc *sc;
 	int fmt, pol, clk;
+	uint32_t ctl, fat0, chsel;
+	u_int offset;
 
 	sc = device_get_softc(dev);
 
@@ -361,7 +420,78 @@ aw_i2s_dai_init(device_t dev, uint32_t format)
 	pol = AUDIO_DAI_FORMAT_POLARITY(format);
 	clk = AUDIO_DAI_FORMAT_CLOCK(format);
 
-	printf("TODO: implement me %s\n", __func__);
+	ctl = I2S_READ(sc, DA_CTL);
+	fat0 = I2S_READ(sc, DA_FAT0);
+
+	if (I2S_TYPE(sc) == SUNXI_I2S_SUN4I) {
+		fat0 &= ~DA_FAT0_FMT_MASK;
+		switch (fmt) {
+		case AUDIO_DAI_FORMAT_I2S:
+			fat0 |= DA_FMT_I2S;
+			break;
+		case AUDIO_DAI_FORMAT_RJ:
+			fat0 |= DA_FMT_RJ;
+			break;
+		case AUDIO_DAI_FORMAT_LJ:
+			fat0 |= DA_FMT_LJ;
+			break;
+		default:
+			return EINVAL;
+		}
+		ctl &= ~DA_CTL_PCM;
+	} else {
+		ctl &= ~DA_CTL_MODE_SEL_MASK;
+		switch (fmt) {
+		case AUDIO_DAI_FORMAT_I2S:
+			ctl |= DA_CTL_MODE_SEL_LJ;
+			offset = 1;
+			break;
+		case AUDIO_DAI_FORMAT_LJ:
+			ctl |= DA_CTL_MODE_SEL_LJ;
+			offset = 0;
+			break;
+		case AUDIO_DAI_FORMAT_RJ:
+			ctl |= DA_CTL_MODE_SEL_RJ;
+			offset = 0;
+			break;
+		case AUDIO_DAI_FORMAT_DSPA:
+			ctl |= DA_CTL_MODE_SEL_PCM;
+			offset = 1;
+			break;
+		case AUDIO_DAI_FORMAT_DSPB:
+			ctl |= DA_CTL_MODE_SEL_PCM;
+			offset = 0;
+			break;
+		default:
+			return EINVAL;
+		}
+
+		chsel = I2S_READ(sc, sc->cfg->txchsel);
+		chsel &= ~DA_CHSEL_OFFSET_MASK;
+		chsel |= DA_CHSEL_OFFSET(offset);
+		I2S_WRITE(sc, sc->cfg->txchsel, chsel);
+
+		chsel = I2S_READ(sc, sc->cfg->rxchsel);
+		chsel &= ~DA_CHSEL_OFFSET_MASK;
+		chsel |= DA_CHSEL_OFFSET(offset);
+		I2S_WRITE(sc, sc->cfg->rxchsel, chsel);
+	}
+
+	fat0 &= ~(DA_FAT0_LRCP_MASK|DA_FAT0_BCP_MASK);
+	if (I2S_TYPE(sc) == SUNXI_I2S_SUN4I) {
+		if (AUDIO_DAI_POLARITY_INVERTED_BCLK(pol))
+			fat0 |= DA_BCP_INVERTED;
+		if (AUDIO_DAI_POLARITY_INVERTED_FRAME(pol))
+			fat0 |= DA_LRCP_INVERTED;
+	} else {
+		if (AUDIO_DAI_POLARITY_INVERTED_BCLK(pol))
+			fat0 |= DA_BCP_INVERTED;
+		if (!AUDIO_DAI_POLARITY_INVERTED_FRAME(pol))
+			fat0 |= DA_LRCP_INVERTED;
+
+		fat0 &= ~DA_FAT0_LRCK_PERIOD_MASK;
+		fat0 |= DA_FAT0_LRCK_PERIOD(32 - 1);
+	}
 
 	return (0);
 }
@@ -460,16 +590,60 @@ static int
 aw_i2s_dai_set_sysclk(device_t dev, unsigned int rate, int dai_dir)
 {
 	struct aw_i2s_softc *sc;
-	clk_t parent;
+	int bclk_val, mclk_val;
+	uint32_t val;
 	int error;
 
 	sc = device_get_softc(dev);
 
-	// to trigger re-parenting
-	clk_set_freq(sc->clk, rate, 0);
+	error = clk_set_freq(sc->clk, AW_I2S_CLK_RATE, CLK_SET_ROUND_DOWN);
+	if (error != 0) {
+		device_printf(sc->dev,
+		    "couldn't set mod clock rate to %u Hz: %d\n", AW_I2S_CLK_RATE, error);
+		return error;
+	}
+	error = clk_enable(sc->clk);
+	if (error != 0) {
+		device_printf(sc->dev,
+		    "couldn't enable mod clock: %d\n", error);
+		return error;
+	}
 
-	if (clk_get_parent(sc->clk, &parent) == 0)
-		error = clk_set_freq(parent, rate, CLK_SET_ROUND_DOWN);
+	const u_int bclk_prate = I2S_TYPE(sc) == SUNXI_I2S_SUN4I ? rate : AW_I2S_CLK_RATE;
+
+	const u_int bclk_div = bclk_prate / (2 * 32 * AW_I2S_SAMPLE_RATE);
+	const u_int mclk_div = AW_I2S_CLK_RATE / rate;
+
+	if (I2S_TYPE(sc) == SUNXI_I2S_SUN4I) {
+		bclk_val = sunxi_i2s_div_to_regval(sun4i_i2s_bclk_divmap,
+		    nitems(sun4i_i2s_bclk_divmap), bclk_div);
+		mclk_val = sunxi_i2s_div_to_regval(sun4i_i2s_mclk_divmap,
+		    nitems(sun4i_i2s_mclk_divmap), mclk_div);
+	} else {
+		bclk_val = sunxi_i2s_div_to_regval(sun8i_i2s_divmap,
+		    nitems(sun8i_i2s_divmap), bclk_div);
+		mclk_val = sunxi_i2s_div_to_regval(sun8i_i2s_divmap,
+		    nitems(sun8i_i2s_divmap), mclk_div);
+	}
+	if (bclk_val == -1 || mclk_val == -1) {
+		device_printf(sc->dev, "couldn't configure bclk/mclk dividers\n");
+		return EIO;
+	}
+
+	val = I2S_READ(sc, DA_CLKD);
+	if (I2S_TYPE(sc) == SUNXI_I2S_SUN4I) {
+		val |= DA_CLKD_MCLKO_EN_SUN4I;
+		val &= ~DA_CLKD_BCLKDIV_SUN4I_MASK;
+		val |= DA_CLKD_BCLKDIV_SUN4I(bclk_val);
+	} else {
+		val |= DA_CLKD_MCLKO_EN_SUN8I;
+		val &= ~DA_CLKD_BCLKDIV_SUN8I_MASK;
+		val |= DA_CLKD_BCLKDIV_SUN8I(bclk_val);
+	}
+	val &= ~DA_CLKD_MCLKDIV_MASK;
+	val |= DA_CLKD_MCLKDIV(mclk_val);
+	I2S_WRITE(sc, DA_CLKD, val);
+
 
 	return (0);
 }
